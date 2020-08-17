4e6e44b0d2ceb8f37bf19029dc8fe987d6101a71
sameasychen <45924533+sameasychen@users.noreply.github.com>
2020-08-17 16:49:58 -0400

    Updates

---
+++ precache-manifest.c7153aa2c2e9083e35606edc53701543.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8cc471b86d10e192f04a9547ee3e14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0d07ed84c8ec7803524",
+    "url": "./static/css/main.b89b396e.chunk.css"
+  },
+  {
+    "revision": "93ebaa9f8e80abdec4a8",
+    "url": "./static/js/2.d3d4c31b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d3d4c31b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d07ed84c8ec7803524",
+    "url": "./static/js/main.da1edf53.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "328657fdf26fc8e7b39e65bf1c791b9d",
+    "url": "./static/media/OtherBanner.328657fd.jpg"
+  },
+  {
+    "revision": "c5e570dd4dea1838e57782d6387d74d4",
+    "url": "./static/media/headerbg.c5e570dd.jpg"
+  },
+  {
+    "revision": "fffac679b62e78989d20b666df126776",
+    "url": "./static/media/homebanner.fffac679.jpg"
+  },
+  {
+    "revision": "60d847f87b8601bf3642b4e610f2775a",
+    "url": "./static/media/react_gif.60d847f8.gif"
+  }
+]);
